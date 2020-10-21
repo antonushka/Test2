dc88392af9f9428494a906825ed9e928e3b05a72
Anna Antonova <antonushka98@mail.ru>
2020-10-21 12:41:05 +0400

    Initial Commit

---
+++ Test2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test2
+//
+//  Created by Анна Антонова on 21.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
